2e5848572b10340029ba7c719d9422240417d4d5
ritikkoul <61817094+ritikkoul0@users.noreply.github.com>
2025-05-24 12:07:12 +0530

    Add files via upload

---
+++ handlers/handlers_structure.go
@@ -0,0 +1,8 @@
+package handlers
+
+
+type SignupInput struct {
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+}
