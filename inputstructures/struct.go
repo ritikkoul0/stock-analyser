28b591e9b9447a5be876c5379a5e58ec912ed7a6
ritikkoul <61817094+ritikkoul0@users.noreply.github.com>
2025-05-25 19:58:33 +0530

    Add files via upload

--- inputstructures/struct.go
+++ inputstructures/struct.go
@@ -5,8 +5,3 @@ type SignupInput struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=6"`
 }
-
-type LoginInput struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6"`
-}
--- routers/routers.go
+++ routers/routers.go
@@ -16,9 +16,18 @@ func SetupRouter() *gin.Engine {
 	baseRouter := gin.Default()
 	baseMiddleware := []gin.HandlerFunc{}
 	baseRouter.Use(baseMiddleware...)
-
 	baseRouter.POST(ssoSignupUrl, handlers.UserSignup)
-	baseRouter.POST(ssoLoginUrl, handlers.UserLogin)
+
+	//public ipo api's
+	v1 := baseRouter.Group("/api/v1")
+	{
+		ipo := v1.Group("/ipo")
+		{
+			ipo.GET("/upcoming", handlers.GetUpcomingIPOs)
+			ipo.GET("/open", handlers.GetOpenIPOs)
+			ipo.GET("/closed", handlers.GetClosedIPOs)
+		}
+	}
 
 	// Apply middleware to router
 	backendMiddleware := []gin.HandlerFunc{
